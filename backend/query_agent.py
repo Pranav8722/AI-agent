f3a13246a70fbfe267399a1911a86277647f2023
Pranav8722 <pranavbadaskar06@gmail.com>
2025-09-28 17:54:49 +0530

    some path changes

--- backend/query_agent.py
+++ backend/query_agent.py
@@ -4,7 +4,8 @@ import re
 import sqlite3
 from pathlib import Path
 from groq import Groq
-from file_utils import execute_sql, generate_plot
+from backend.file_utils import execute_sql, generate_plot
+
 import pandas as pd
 
 BASE_DIR = Path(__file__).parent
